7c6d75dfbcbcfed1289da56ca653874dcea03a7a
Mark Johnson <mj464@cam.ac.uk>
2016-11-11 15:12:40 +0000

    Made compile.sh executable, and corrected its bugs.

--- compile.sh
+++ compile.sh
@@ -5,35 +5,35 @@
 # harmonic
 # --------------------
 # define directories
-src=src/harmonic/programs/
+src=src/harmonic/
 mod=mod/
 obj=obj/
 bin=bin/
 
 # copy shell scripts
-cp src/harmonic/scripts/*.sh bin/
+cp src/harmonic/*.sh bin/
 
 # list programs
 programs=(combine_forces compare_kpoints construct_finite_displacement construct_matrix_force_cnsts construct_supercell convert_forces_from_Rybohr_to_eVang equilibrium_frac fourier_interpolation generate_kgrid generate_supercell_kpoint_mesh_qe generate_supercells lte lte_lower)
 
 # compile programs
 for program in ${programs[@]}; do
-	gfortran $src$program -J$mod -o $bin$program -lblas -llapack
+	gfortran $src$program.f90 -J$mod -o $bin$program -lblas -llapack
 done
 
 # --------------------
 # quadratic
 # --------------------
 # change src directory
-src=src/quadratic/programs/
+src=src/quadratic/
 
 # copy shell scripts
-cp src/quadratic/scripts/*.sh bin/
+cp src/quadratic/*.sh bin/
 
 # list programs
 programs=(band_folding calculate_anharmonic calculate_bs calculate_gap generate_amplitudes generate_quadratic_configurations generate_sc_path quadratic_spline vscf_1d)
 
 # compile programs
 for program in ${programs[@]}; do
-	gfortran $src$program -J$mod -o $bin$program -lblas -llapack
+	gfortran $src$program.f90 -J$mod -o $bin$program -lblas -llapack
 done
