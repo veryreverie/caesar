f5ab5b21d9ab0cf724266a85cc49b6a82b127476
Mark Johnson <mj464@cam.ac.uk>
2016-11-14 18:48:18 +0000

    Added basic help text for top-level harmonic scripts.

--- src/caesar.f90
+++ src/caesar.f90
@@ -37,7 +37,74 @@ program caesar
     write(*,*) "No arguments given. For help, call caesar -h"
     stop
   elseif (args(1) == "-h" .or. args(1) == "--help") then
-    write(*,*) "[Placeholder help text]"
+    write(*,*) "caesar [-h] [option]"
+    write(*,*) ""
+    write(*,*) "-h :"
+    write(*,*) "  Displays this help text"
+    write(*,*) ""
+    write(*,*) "option : utilities :"
+    write(*,*) "  hartree_to_eV :"
+    write(*,*) "    Provides a Hartree to eV calculator"
+    write(*,*) ""
+    write(*,*) "option : harmonic calculations :"
+    write(*,*) "  setup_harmonic :"
+    write(*,*) "    Sets up calculation"
+    write(*,*) "  convert_harmonic :"
+    write(*,*) "    Converts calculation to specific code"
+    write(*,*) "    Choices are castep, vasp and quantum espresso"
+    write(*,*) "    Should be called after setup_harmonic"
+    write(*,*) "  tcm_cluster_run_harmonic :"
+    write(*,*) "    Runs calculation on the TCM cluster"
+    write(*,*) "    Should be called after convert_harmonic"
+    write(*,*) "  rutgers_run_harmonic :"
+    write(*,*) "    Runs calculations"
+    write(*,*) "    Should be called after convert_harmonic"
+    write(*,*) "  lte_harmonic :"
+    write(*,*) "    Runs harmonic calculations"
+    write(*,*) "    Should be run after one of the run_harmonic options"
+    write(*,*) "  clear_all :"
+    write(*,*) "    Deletes all temporary files and folders"
+    write(*,*) ""
+    write(*,*) "option : quadratic calculations :"
+    stop
+  elseif (args(1) == "combine_forces") then
+    write(*,*) "option selected"
+    stop
+  elseif (args(1) == "compare_kpoints") then
+    write(*,*) "option selected"
+    stop
+  elseif (args(1) == "construct_finite_displacement") then
+    write(*,*) "option selected"
+    stop
+  elseif (args(1) == "construct_matrix_force_cnsts") then
+    write(*,*) "option selected"
+    stop
+  elseif (args(1) == "construct_supercell") then
+    write(*,*) "option selected"
+    stop
+  elseif (args(1) == "") then
+    write(*,*) "option selected"
+    stop
+  elseif (args(1) == "") then
+    write(*,*) "option selected"
+    stop
+  elseif (args(1) == "") then
+    write(*,*) "option selected"
+    stop
+  elseif (args(1) == "") then
+    write(*,*) "option selected"
+    stop
+  elseif (args(1) == "") then
+    write(*,*) "option selected"
+    stop
+  elseif (args(1) == "") then
+    write(*,*) "option selected"
+    stop
+  elseif (args(1) == "") then
+    write(*,*) "option selected"
+    stop
+  elseif (args(1) == "") then
+    write(*,*) "option selected"
     stop
   else
     write(*,*) "Unrecognised argument : "//args(1)
