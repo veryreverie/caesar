5b1f3378f0e3e9f589505b466e1b36f758bc7181
Mark Johnson <mj464@cam.ac.uk>
2016-11-15 14:55:43 +0000

    Added call functionality to caesar, such that e.g. '$ caesar setup_harmonic' calls setup_harmonic.sh

--- src/caesar.f90
+++ src/caesar.f90
@@ -5,9 +5,9 @@ contains
   function command_line_args() result(o_args)
     implicit none
 
-    integer                        :: i
-    integer                        :: arg_count
-    character(len=32), allocatable :: o_args(:)
+    integer                        :: i         ! loop index
+    integer                        :: arg_count ! no. of command line args
+    character(len=32), allocatable :: o_args(:) ! return value
     
     ! read in the number of arguments
     arg_count = iargc()
@@ -27,16 +27,26 @@ program caesar
   use utils, only : command_line_args
   implicit none
   
-  integer           :: i
-  character(len=32), allocatable :: args(:)
+  integer                        :: i             ! loop index
+  character(len=32), allocatable :: args(:)       ! command line arguments
+  character(len=:),  allocatable :: arg           ! command line argument
+  integer                        :: return_status ! system() status
 
   ! read in command line arguments
   args = command_line_args()
   
   if (size(args) == 0) then
     write(*,*) "No arguments given. For help, call caesar -h"
+    deallocate(args)
     stop
-  elseif (args(1) == "-h" .or. args(1) == "--help") then
+  else
+    allocate(character(len=len(trim(args(1)))) :: arg)
+    arg = trim(args(1))
+    write(*,*) len(arg)
+    write(*,*) arg
+  endif
+  
+  if (arg == "-h" .or. arg == "--help") then
     write(*,*) "caesar [-h] [option]"
     write(*,*) ""
     write(*,*) "-h :"
@@ -66,51 +76,149 @@ program caesar
     write(*,*) "    Deletes all temporary files and folders"
     write(*,*) ""
     write(*,*) "option : quadratic calculations :"
-    stop
-  elseif (args(1) == "combine_forces") then
-    write(*,*) "option selected"
-    stop
-  elseif (args(1) == "compare_kpoints") then
-    write(*,*) "option selected"
-    stop
-  elseif (args(1) == "construct_finite_displacement") then
-    write(*,*) "option selected"
-    stop
-  elseif (args(1) == "construct_matrix_force_cnsts") then
-    write(*,*) "option selected"
-    stop
-  elseif (args(1) == "construct_supercell") then
-    write(*,*) "option selected"
-    stop
-  elseif (args(1) == "") then
-    write(*,*) "option selected"
-    stop
-  elseif (args(1) == "") then
-    write(*,*) "option selected"
-    stop
-  elseif (args(1) == "") then
-    write(*,*) "option selected"
-    stop
-  elseif (args(1) == "") then
-    write(*,*) "option selected"
-    stop
-  elseif (args(1) == "") then
-    write(*,*) "option selected"
-    stop
-  elseif (args(1) == "") then
-    write(*,*) "option selected"
-    stop
-  elseif (args(1) == "") then
-    write(*,*) "option selected"
-    stop
-  elseif (args(1) == "") then
-    write(*,*) "option selected"
-    stop
+    write(*,*) "  [quadratic help text yet to be written]"
+  ! Wrappers for Fortran 
+  elseif (arg == "band_folding") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg)
+  elseif (arg == "calculate_anharmonic") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg)
+  elseif (arg == "calculate_bs") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg)
+  elseif (arg == "calculate_gap") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg)
+  elseif (arg == "combine_forces") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg)
+  elseif (arg == "compare_kpoints") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg)
+  elseif (arg == "construct_finite_displacement") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg)
+  elseif (arg == "construct_matrix_force_cnsts") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg)
+  elseif (arg == "construct_supercell") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg)
+  elseif (arg == "convert_forces_from_Rybohr_to_eVang") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg)
+  elseif (arg == "equilibrium_frac") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg)
+  elseif (arg == "fourier_interpolation") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg)
+  elseif (arg == "generate_amplitudes") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg)
+  elseif (arg == "generate_kgrid") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg)
+  elseif (arg == "generate_quadratic_configurations") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg)
+  elseif (arg == "generate_sc_path") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg)
+  elseif (arg == "generate_supercells") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg)
+  elseif (arg == "lte") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg)
+  elseif (arg == "lte_lower") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg)
+  elseif (arg == "quadratic_spline") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg)
+  elseif (arg == "vscf_1d") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg)
+  ! shell scripts
+  elseif (arg == "anharmonic") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg//".sh")
+  elseif (arg == "bs_quadratic") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg//".sh")
+  elseif (arg == "clear_all") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg//".sh")
+  elseif (arg == "convert_harmonic") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg//".sh")
+  elseif (arg == "convert_quadratic") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg//".sh")
+  elseif (arg == "dyn_mats") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg//".sh")
+  elseif (arg == "eigenval_castep_to_bands") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg//".sh")
+  elseif (arg == "eigenval_vasp_to_bands") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg//".sh")
+  elseif (arg == "fetch_forces_castep") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg//".sh")
+  elseif (arg == "fetch_forces_qe") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg//".sh")
+  elseif (arg == "hartree_to_eV") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg//".sh")
+  elseif (arg == "lte_harmonic") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg//".sh")
+  elseif (arg == "rutgers_run_harmonic") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg//".sh")
+  elseif (arg == "setup_harmonic") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg//".sh")
+  elseif (arg == "setup_quadratic") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg//".sh")
+  elseif (arg == "structure_to_castep") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg//".sh")
+  elseif (arg == "structure_to_qe") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg//".sh")
+  elseif (arg == "structure_to_vasp") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg//".sh")
+  elseif (arg == "tcm_cleanup_anharmonic") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg//".sh")
+  elseif (arg == "tcm_cleanup_bs") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg//".sh")
+  elseif (arg == "tcm_cluster_run_harmonic") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg//".sh")
+  elseif (arg == "tcm_cluster_run_quadratic") then
+    write(*,*) "option selected: ", arg
+    return_status = system(arg//".sh")
+  ! python scripts
+  elseif (arg == "get_kpoints") then
+    write(*,*) "option selected: ", arg
+    write(*,*) arg//".py"
+    return_status = system(arg//".py")
+  ! unrecognised argument
   else
-    write(*,*) "Unrecognised argument : "//args(1)
-    stop
+    write(*,*) "Unrecognised argument : "//arg
   endif
   
-  deallocate (args)
+  deallocate(arg)
+  deallocate(args)
 
 end program
