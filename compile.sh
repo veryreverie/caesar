55509c0d6aefd1c0fcd48ed12578f0ec12361ff6
Mark Johnson <mj464@cam.ac.uk>
2016-11-15 17:48:41 +0000

    Modified compile.sh to allow linking between multiple object files, in preparation for moving modules to separate files. Moved utils module of caesar.f90 to utils.f90.

--- compile.sh
+++ compile.sh
@@ -10,47 +10,58 @@ for directory in ${directories[@]}; do
 done
 
 # define directories
-src=src/
-mod=mod/
-obj=obj/
-bin=bin/
+bdir=bin
+ddir=doc
+mdir=mod
+odir=obj
+sdir=src
+
+# define compiler
+cf90=gfortran
+
+# copy shell and python scripts
+cp $sdir/harmonic/*.sh $bdir/
+cp $sdir/quadratic/*.sh $bdir/
+cp $sdir/quadratic/*.py $bdir/
+
+# list programs
+harmonic_programs=(combine_forces compare_kpoints construct_finite_displacement construct_matrix_force_cnsts construct_supercell convert_forces_from_Rybohr_to_eVang equilibrium_frac fourier_interpolation generate_kgrid generate_supercell_kpoint_mesh_qe generate_supercells lte lte_lower)
+
+quadratic_programs=(band_folding calculate_anharmonic calculate_bs calculate_gap generate_amplitudes generate_quadratic_configurations generate_sc_path quadratic_spline vscf_1d)
+
+programs=(utils)
+
+# list objects for linking
+objs=()
+for program in ${programs[@]}; do
+  objs+=($odir/$program.o)
+done
+
+# compile objects
+for program in ${harmonic_programs[@]}; do
+  $cf90 -c $sdir/harmonic/$program.f90 -J$mdir/ -o$odir/$program.o
+done
+
+for program in ${quadratic_programs[@]}; do
+  $cf90 -c $sdir/quadratic/$program.f90 -J$mdir/ -o$odir/$program.o
+done
+
+for program in ${programs[@]}; do
+  $cf90 -c $sdir/$program.f90 -J$mdir/ -o$odir/$program.o
+done
+
+program=caesar
+$cf90 -c $sdir/$program.f90 -J$mdir/ -o$odir/$program.o
+
+# link objects
+for program in ${harmonic_programs[@]}; do
+	$cf90 $odir/$program.o -J$mdir/ -o $bdir/$program $objs -lblas -llapack
+done
+
+for program in ${quadratic_programs[@]}; do
+	$cf90 $odir/$program.o -J$mdir/ -o $bdir/$program $objs -lblas -llapack
+done
 
 # compile main caesar program
-gfortran ${src}caesar.f90 -J$mod -o ${bin}caesar -lblas -llapack
-
-# --------------------
-# harmonic
-# --------------------
-# define directories
-src=src/harmonic/
-
-# copy shell scripts
-cp src/harmonic/*.sh bin/
-
-# list programs
-programs=(combine_forces compare_kpoints construct_finite_displacement construct_matrix_force_cnsts construct_supercell convert_forces_from_Rybohr_to_eVang equilibrium_frac fourier_interpolation generate_kgrid generate_supercell_kpoint_mesh_qe generate_supercells lte lte_lower)
-
-# compile programs
-for program in ${programs[@]}; do
-  gfortran -c $src$program.f90 -J$mod -o$obj$program.o
-	gfortran $obj$program.o -J$mod -o $bin$program -lblas -llapack
-done
-
-# --------------------
-# quadratic
-# --------------------
-# change src directory
-src=src/quadratic/
-
-# copy shell scripts
-cp src/quadratic/*.sh bin/
-cp src/quadratic/*.py bin/
-
-# list programs
-programs=(band_folding calculate_anharmonic calculate_bs calculate_gap generate_amplitudes generate_quadratic_configurations generate_sc_path quadratic_spline vscf_1d)
-
-# compile programs
-for program in ${programs[@]}; do
-  gfortran -c $src$program.f90 -J$mod -o$obj$program.o
-	gfortran $obj$program.o -J$mod -o $bin$program -lblas -llapack
-done
+program=caesar
+$cf90 $odir/$program.o -J$mdir/ -o$bdir/$program $objs -lblas -llapack
--- src/caesar.f90
+++ src/caesar.f90
@@ -1,28 +1,3 @@
-module utils
-contains
-
-  ! returns an array containing the command line arguments.
-  function command_line_args() result(o_args)
-    implicit none
-
-    integer                        :: i         ! loop index
-    integer                        :: arg_count ! no. of command line args
-    character(len=32), allocatable :: o_args(:) ! return value
-    
-    ! read in the number of arguments
-    arg_count = iargc()
-    ! allocate the return array
-    allocate (o_args(arg_count))
-    ! read the arguments into the array
-    do i=1,arg_count
-      call getarg(i, o_args(i))
-    enddo
-    
-    return
-  end function
-
-end module
-
 program caesar
   use utils, only : command_line_args
   implicit none
