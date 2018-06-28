a8a8f8ed04cc4df280900af9da0d203127e5bc6f
Mark Johnson <mj464@cam.ac.uk>
2018-06-28 16:06:24 +0100

    Removed old vector files.

--- src/version.f90
+++ src/version.f90
@@ -8,6 +8,6 @@ contains
 subroutine print_version()
   implicit none
   
-  call print_line('Caesar version: 0.0.180628A (Gaius)')
+  call print_line('Caesar version: 0.0.180628B (Gaius)')
 end subroutine
 end module
