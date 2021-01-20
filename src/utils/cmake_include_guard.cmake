f9229ede62a3d17f6d73a07519b1aa5ab2a12cdf
Mark Johnson <mj464@cam.ac.uk>
2021-01-20 15:26:13 +0000

    Renamed include_guard to cmake_include_guard to avoid a name clash wich more modern CMake versions.

--- src/utils/cmake_include_guard.cmake
+++ src/utils/cmake_include_guard.cmake
@@ -5,7 +5,7 @@
 cmake_minimum_required (VERSION 3.5 FATAL_ERROR)
 
 # A macro for include guards in other files.
-macro(include_guard name)
+macro(cmake_include_guard name)
   get_property(already_included GLOBAL PROPERTY ${name}_included SET)
   if(${already_included})
     return()
